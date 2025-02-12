7322b4b527fbf0a20d2ed2e703b08dd37cba0e36
nanda <nandamonsterking@gmail.com>
2025-02-12 08:46:45 +0530

    redis config to handle caching effectively

---
+++ Employee-Management-System/src/main/java/com/example/Employee_Management_System/config/RedisConfig.java
@@ -0,0 +1,24 @@
+package com.example.Employee_Management_System.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        // Defaults (localhost:6379) can be overridden via application.properties
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        return template;
+    }
+}
+
